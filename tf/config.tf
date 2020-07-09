17832c54e15f20d73497af9202b5c7b9bac06ab9
Carlos Carbonell <carlos.carbonelletayo@zooplus.com>
2020-07-09 16:35:29 +0200

    Initialize terraform

---
+++ tf/config.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region  = var.region
+  shared_credentials_file = "/home/carlosc/git/stuff/tl-icalendar/.aws/credentials"
+}
+
+terraform {
+  required_version = ">= 0.12.0"
+
+  backend "s3" {
+    bucket  = "carcaret-terraform-state"
+    key     = "tl-icalendar/terraform.tfstate"
+    region  = "eu-central-1"
+    shared_credentials_file = "/home/carlosc/git/stuff/tl-icalendar/.aws/credentials"
+  }
+}
