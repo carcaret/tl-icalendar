90956c0ce9bf47fe4dce32421b7df533c044e8d6
Carlos Carbonell <carlos.carbonelletayo@zooplus.com>
2020-07-10 10:04:50 +0200

    Attach policy to put object

--- tf/main.tf
+++ tf/main.tf
@@ -22,11 +22,10 @@ data archive_file lambda_package {
 
 resource aws_iam_role lambda_role {
   name = "${var.project-name}_lambda_role"
-  path = "/c/"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource aws_lambda_function article_status {
+resource aws_lambda_function create-calendar {
   filename = data.archive_file.lambda_package.output_path
   function_name = "${var.project-name}_creator"
   role = aws_iam_role.lambda_role.arn
@@ -40,4 +39,26 @@ resource aws_lambda_function article_status {
       BUCKET_NAME = var.bucket-name
     }
   }
+}
+
+data aws_iam_policy_document put_object {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.bucket-name}/*"
+    ]
+  }
+}
+
+resource aws_iam_policy put-calendar-object-policy {
+  name = "${var.project-name}_put-calendar-object-policy"
+  policy = data.aws_iam_policy_document.put_object.json
+}
+
+resource aws_iam_role_policy_attachment put-calendar-object-attachment {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.put-calendar-object-policy.arn
 }
