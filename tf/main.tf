557f3093fd2293ebf2fed047217a778d9cb2c3b7
Carlos Carbonell <carlos.carbonelletayo@zooplus.com>
2020-07-11 11:16:42 +0200

    Add lambda alarm

--- tf/main.tf
+++ tf/main.tf
@@ -21,7 +21,7 @@ resource aws_s3_bucket sc2-calendar {
   policy = data.aws_iam_policy_document.bucket-public-read.json
 }
 
-data aws_iam_policy_document assume_role {
+data aws_iam_policy_document assume-role {
   statement {
     effect = "Allow"
     actions = [
@@ -40,15 +40,15 @@ data archive_file lambda_package {
   output_path = "${path.module}/../dist/lambda-package.zip"
 }
 
-resource aws_iam_role lambda_role {
-  name = "${var.project-name}_lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource aws_iam_role lambda-role {
+  name = "${var.project-name}_lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume-role.json
 }
 
 resource aws_lambda_function create-calendar {
   filename = data.archive_file.lambda_package.output_path
   function_name = "${var.project-name}_creator"
-  role = aws_iam_role.lambda_role.arn
+  role = aws_iam_role.lambda-role.arn
   handler = "tl_icalendar.event_handler"
   source_code_hash = data.archive_file.lambda_package.output_base64sha256
   runtime = "python3.6"
@@ -61,7 +61,7 @@ resource aws_lambda_function create-calendar {
   }
 }
 
-data aws_iam_policy_document put_object {
+data aws_iam_policy_document put-object {
   statement {
     effect = "Allow"
     actions = [
@@ -75,11 +75,11 @@ data aws_iam_policy_document put_object {
 
 resource aws_iam_policy put-calendar-object-policy {
   name = "${var.project-name}_put-calendar-object-policy"
-  policy = data.aws_iam_policy_document.put_object.json
+  policy = data.aws_iam_policy_document.put-object.json
 }
 
 resource aws_iam_role_policy_attachment put-calendar-object-attachment {
-  role = aws_iam_role.lambda_role.name
+  role = aws_iam_role.lambda-role.name
   policy_arn = aws_iam_policy.put-calendar-object-policy.arn
 }
 
@@ -100,4 +100,20 @@ resource aws_lambda_permission calendar-trigger-permission {
   function_name = aws_lambda_function.create-calendar.function_name
   principal = "events.amazonaws.com"
   source_arn = aws_cloudwatch_event_rule.calendar-trigger.arn
+}
+
+resource aws_sns_topic alarms {
+  name = "${var.project-name}-alarms"
+}
+
+resource aws_cloudwatch_metric_alarm lambda-error-alarm {
+  alarm_name                = "${aws_lambda_function.create-calendar.function_name}-error-alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "Errors"
+  namespace                 = "AWS/Lambda"
+  period                    = "60"
+  statistic                 = "SampleCount"
+  threshold                 = "1"
+  alarm_actions             = [ aws_sns_topic.alarms.arn ]
 }
